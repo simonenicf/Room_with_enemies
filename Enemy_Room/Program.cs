852de463822b288d976d89e3deac063a8edcfe19
michelle <31318@ma-web.nl>
2021-10-03 10:13:43 +0200

    finished enemy in room

---
+++ Enemy_Room/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enemy_Room
+{
+    class Program
+    {
+        static void Main()
+        {
+            Room Kamer = new Room();
+            Kamer.callAllEnemyLogic();
+        }    
+    }
+
+    class Room
+    {
+        public List<Enemy> enemies = new List<Enemy>()
+        {
+            new Enemy(),
+            new Enemy(),
+            new Enemy()
+        };
+        
+        public void callAllEnemyLogic()
+        {
+            foreach (Enemy aEnemy in enemies)
+            {
+                aEnemy.logic();
+            }
+        }
+
+    }
+
+    class Enemy
+    {
+       public void logic()
+        {
+            // checks if this funcion gets called
+            Console.WriteLine("works");
+        }
+
+    }
+}
